75128ccd868e4d402533e8d1cad3497a00512eb6
angela34a <andela.cukusic@yahoo.com>
2023-08-20 17:46:14 +0200

    add analysis script

--- Analysis/Constrained.R
+++ Analysis/Constrained.R
@@ -1,115 +1,161 @@
+
+# pca  of env vars ####
+
+df_alpha <- metadata %>%  
+  column_to_rownames("sample_season") %>% 
+  dplyr::select(where(is.numeric)) %>% 
+  dplyr::select(-c("NH4", # many NAs
+                   "X", "Y",     # coordinates
+                   "Extraction_depth", "Watercolumn", "GW_Table")) %>% 
+  na.omit() %>% 
+  scale(center = T, scale = T) %>% 
+  as.data.frame()
+cor_matrix <- cor(df_alpha)
+highly_correlated <- caret::findCorrelation(cor_matrix, cutoff = 0.9) 
+# none
+# df_alpha <- df_alpha[, - highly_correlated] 
+
+
+# lets see if any env vars can be packed neatly in PCs
+pca_env <- prcomp(df_alpha, scale = FALSE)
+biplot(pca_env)
+
+
+ggplot2::autoplot(pca_env, 
+                  
+                  data = 
+                    (
+                      df_alpha %>% 
+                        rownames_to_column("sample_season") %>% 
+                        left_join(., metadata[,c("category", "sample_season")], 
+                                  by = "sample_season") 
+                    ), 
+                  
+                  colour = 'category',
+                  size=5, 
+                  alpha = 0.2,
+                  loadings = TRUE, 
+                  loadings.colour = "black", 
+                  loadings.label = TRUE, 
+                  loadings.label.size = 4.3, 
+                  loadings.label.colour = "black",
+                  loadings.label.repel = TRUE) +
+  
+  geom_vline(xintercept = c(0), color = "grey70", linetype = 2) +
+  geom_hline(yintercept = c(0), color = "grey70", linetype = 2) +
+  scale_color_manual(values = pal3)+
+  theme(legend.background = element_blank(),
+        legend.box.background = element_rect(colour = "gray"))  -> pca_plot
+
+
+pca_plot
+
+# summary of first three PCs
+summary(pca_env)$importance %>% 
+  as.data.frame() %>% 
+  dplyr::select("PC1", "PC2", "PC3", "PC4")
+
+
+# pca loadings ####
+# Extract the loadings from the PCA result and convert to tidy format 
+loadings_df <-   tidy(pca_env, matrix = "rotation")
+
+
+# PC1
+# Create the ggplot2 object for plotting the loadings
+loadings_df %>% filter(PC == "1") %>% 
+  #x=reorder(class,-amount,sum)
+  ggplot( aes(x = reorder(column, -value), y = value, group = factor(column))) +
+  geom_bar(stat = "identity", position = "dodge", 
+           fill= "gray90", color = "gray20") +
+  geom_text(aes(label = column), position = position_dodge(width = 0.9), 
+            #vjust = "inward",  
+            angle = 90 , hjust = "inward") +
+  labs(x = "Variables of PC1", y = "Loadings") +
+  theme_minimal() +
+  theme(axis.text.x = element_blank(),
+        legend.position = "none")  -> plot_pca1
+
+# PC2 
+loadings_df %>% filter(PC == "2") %>% 
+  #x=reorder(class,-amount,sum)
+  ggplot( aes(x = reorder(column, -value), y = value, group = factor(column))) +
+  geom_bar(stat = "identity", position = "dodge", 
+           fill= "gray90", color = "gray20") +
+  geom_text(aes(label = column), position = position_dodge(width = 0.9), 
+            #vjust = "inward",  
+            angle = 90 , hjust = "inward") +
+  labs(x = "Variables of PC2", y = "Loadings") +
+  theme_minimal() +
+  theme(axis.text.x = element_blank(),
+        legend.position = "none") -> plot_pca2
+
+# PC3
+loadings_df %>% filter(PC == "3") %>% 
+  #x=reorder(class,-amount,sum)
+  ggplot( aes(x = reorder(column, -value), y = value, group = factor(column))) +
+  geom_bar(stat = "identity", position = "dodge", 
+           fill= "gray90", color = "gray20") +
+  geom_text(aes(label = column), position = position_dodge(width = 0.9), 
+            #vjust = "inward",  
+            angle = 90 , hjust = "inward") +
+  labs(x = "Variables of PC3", y = "Loadings") +
+  theme_minimal() +
+  theme(axis.text.x = element_blank(),
+        legend.position = "none") -> plot_pca3
+
+plot_pca1 + plot_pca2 + plot_pca3
+
+
+# constraints ####
+
 # env clean
+# df_alpha - already scaled and selected the important variables in other script
 
-new_metadata %>% dplyr::select(where(is.numeric)) %>% 
-  dplyr::select(!c("Fe", "NO3", "NH3", "DOM_bix", "X", "Y")) %>% 
-  scale() %>% as.data.frame()  -> env.dat
-
-
-
-# aggregate to class
-sp.dat <- asv_aggregated # already done for nmds
-
+# aggregate to genus
+# asv_aggregated - already done for nmds
 
 # asv clean
-sp.dat [, which  ( colnames(sp.dat) %in% rownames(env.dat)) ] -> sp.dat
+# because some samples get removed because of NAs 
+asv_aggregated [, which  ( colnames(asv_aggregated) %in% rownames(df_alpha)) ] -> sp.dat
 
 # transform hellinger (or clr sometimes)
 decostand(sp.dat, method = "hellinger") -> sp.dat
 #clr(sp.dat) -> sp.dat
 
-
-simpleRDA <- capscale(t(sp.dat) ~  ., data=env.dat ,
+# 1. first model
+simpleRDA <- capscale(t(sp.dat) ~  ., data = df_alpha ,
                       distance = "bray",
                       scaling = "sites")
-
-# summary(simpleRDA)
-#9.6% constrained but look at vif
-
+# take only that are vif<10
 env_vars_low_vif <- vif.cca(simpleRDA) %>% 
   as.data.frame() %>% 
   dplyr::rename("env" = ".") %>% 
   dplyr::filter(env < 10) 
 
-env.dat[,which(colnames(env.dat) %in%  rownames(env_vars_low_vif) )] -> env.dat
+# make new environmental data with only low vif variables
+df_alpha[,which(colnames(df_alpha) %in%  rownames(env_vars_low_vif) )] -> env.dat
 
 
-# without them
+# 2. model without them
 simpleRDA <- capscale(t(sp.dat) ~  ., 
                       data=  env.dat ,
                       distance = "bray",
                       scaling = "sites")
 
-#summary(simpleRDA) 
-# now 5.6%
 
+# test all variables and axes
+# canonical axes
+anova.model <- anova.cca(simpleRDA, by='axis', step=1000)  
+# no axes sign
 
-# test it
-
-
-
-# Test of all canonical axes
-anova.model <- anova.cca(simpleRDA, by='axis', step=1000)  # cap1 is sign, cap2 not
-
-# test the whole model
+# the whole model
 anova.model2 <- anova.cca(simpleRDA, step=1000) #is sign
-RsquareAdj(simpleRDA)$adj.r.squared # and explains 1.7%
+RsquareAdj(simpleRDA)$adj.r.squared # and explains <1%
 
 
-# test env paameters
-anova.model3 <- anova(simpleRDA, step=1000, by = "term") #Temp + DOC sign, and K + O2 borderline
+# env parameters
+anova.model3 <- anova(simpleRDA, step=1000, by = "term") 
+# none sign
 
 
-# new model
-simpleRDA <- capscale(t(sp.dat) ~  Temp +  DOC + K + O2, data=env.dat ,
-                      distance = "bray",
-                      scaling = "sites")
-
-#summary(simpleRDA) 
-# now 1.4%
-
-
-
-
-
-
-# vectors
-ccavectors <- as.matrix(scores(simpleRDA, display = "bp", scaling = "sites")*40) %>% 
-  t() %>% as.data.frame() %>% 
-  rename("K**" = "K",
-         "Temp." = "Temp",
-         "O2*" = "O2") %>%
-  t() %>% as.data.frame()
-
-# site coordinates
-site_data <- scores(simpleRDA, display = "sites") %>% 
-  as.data.frame() %>% 
-  cbind(., new_metadata$category) %>% 
-  rename("cat" = "new_metadata$category")
-
-# plotting
-plot_cca <- 
-  site_data %>% 
-  ggplot( aes(x = CAP1, y = CAP2)) +
-  geom_point(aes( color= cat), alpha = 0.2, size = 3) +
-  geom_point(aes( color= cat), shape = 21, size = 3) +  
-  geom_segment(data = ccavectors, aes(x = 0, y = 0, xend = CAP1, yend = CAP2), size = 1.2,
-               arrow = arrow(length = unit(0.5, "cm"))) +
-  geom_vline(xintercept = c(0), color = "grey70", linetype = 2) +
-  geom_hline(yintercept = c(0), color = "grey70", linetype = 2) +
-  geom_text(data = ccavectors, aes(x = CAP1*1.5, y = CAP2*1.5, 
-                                   label = rownames(ccavectors)),
-            #nudge_x = 0.3, nudge_y = 0.3
-            size=6 ) +
-  theme_bw() +
-  scale_fill_manual(values= pal3) +
-  scale_color_manual(values=pal3) +
-  labs(x = "CAP1 [1.6%]", y="CAP2 [0.7%]") +
-  stat_ellipse(aes(color=cat), size = 1, alpha = 0.5) + 
-  #stat_ellipse(aes(fill = cat), geom="polygon", level=0.95, alpha=0.09) +
-  labs( color = "Temperature \ncategory", fill = "Temperature category", 
-        title = "Bray distances of hellinger transformed samples (n = 191), 180 classes.") +
-  
-  theme(legend.background = element_blank(),
-        legend.box.background = element_rect(colour = "gray")) 
-
-plot_cca
